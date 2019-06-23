aee2ae66f5e938885ec0e0c11461665c2cdf2705
calvince <calmosh1421@gmail.com>
2019-06-24 02:42:09 +0300

    created the user interface and added the main method

---
+++ src/main/java/App.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import static spark.Spark.*;
+
+public class App {
+    public static void main(String[]args){
+        staticFileLocation("/public");
+
+    }
+}
