f303f8eab812ecf4288d9ae4c71a761523690039
calvince <calmosh1421@gmail.com>
2019-06-24 04:49:17 +0300

    added getpublished and getcreated methods

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -1,4 +1,62 @@
 package models;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Hero {
+    private String mName;
+    private int mAge;
+    private String mPower;
+    private String mWeakness;
+    private static List<Hero> instances = new ArrayList<>();
+    private int mId;
+    private boolean published;
+    private LocalDateTime createdAt;
+
+
+
+    public Hero(String name,int age, String power, String weakness){
+        this.mName = name;
+        this.mAge = age;
+        this.mPower = power;
+        this.mWeakness = weakness;
+        this.published = false;
+        this.createdAt = LocalDateTime.now();
+        instances.add(this);
+        this.mId = instances.size();
+
+    }
+
+    public String getName(){
+        return this.mName;
+    }
+    public int getmAge(){
+        return this.mAge;
+    }
+    public String getmPower(){
+        return mPower;
+    }
+    public String getmWeakness(){
+        return mWeakness;
+    }
+    public static List<Hero> getAll(){
+        return instances;
+    }
+
+    public static void clearAllHero(){
+        instances.clear();
+    }
+    public int getmId(){
+        return mId;
+    }
+    public static Hero findById(int id){
+        return instances.get(id-1);
+    }
+
+    public boolean getPublished(){
+        return this.published;
+    }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
 }
