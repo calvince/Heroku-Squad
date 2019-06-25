fc0fd9dfa601749571a75156331fcb3fd73fca59
calvince <calmosh1421@gmail.com>
2019-06-25 07:24:50 +0300

    Changed various methods

--- src/test/java/models/HeroTest.java
+++ src/test/java/models/HeroTest.java
@@ -3,9 +3,7 @@ package models;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
 import java.time.LocalDateTime;
-
 import static org.junit.Assert.*;
 
 public class HeroTest {
@@ -13,7 +11,6 @@ public class HeroTest {
     @Before
     public void setUp(){
     }
-
     @After
     public void tearDown()  {
         Hero.clearAllHero();
@@ -32,23 +29,23 @@ public class HeroTest {
     @Test
     public void Hero_instantiatesWith_Age_int(){
         Hero hero = new Hero("Omosh",24,"might","short");
-        assertEquals(24,hero.getmAge());
+        assertEquals(24,hero.getAge());
     }
     @Test
     public void Hero_instantiatesCorrectlyWithPower() {
         Hero hero = new Hero("Omosh",24,"might","short");
-        assertEquals("might", hero.getmPower());
+        assertEquals("might", hero.getPower());
     }
     @Test
     public void Hero_instantiatesCorrectlyWithWeakness_String() {
         Hero hero = new Hero("Omosh",24,"might","short");
-        assertEquals("short", hero.getmWeakness());
+        assertEquals("short", hero.getWeakness());
     }
 
     @Test
     public void AllHeroAreCorrectlyReturned_true() {
-       Hero hero = new Hero("Omosh",24,"might","short");
-       Hero otherHero = new Hero ("Omosh",24,"might","short");
+        Hero hero = new Hero("Omosh",24,"might","short");
+        Hero otherHero = new Hero ("Omosh",24,"might","short");
         assertEquals(2, Hero.getAll().size());
     }
     @Test
@@ -71,7 +68,6 @@ public class HeroTest {
         Hero hero = setupNewPost();
         assertEquals(1, Hero.findById(hero.getmId()).getmId());
     }
-
 
     @Test
     public void getId_heroesInitiateWithAnID_1(){
