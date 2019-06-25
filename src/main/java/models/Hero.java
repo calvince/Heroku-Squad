8fb2b16c7b1eed214cbccb4335a0ead3c57f304d
calvince <calmosh1421@gmail.com>
2019-06-25 07:24:30 +0300

    edited getters

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -4,43 +4,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Hero {
-    private String mName;
-    private int mAge;
-    private String mPower;
-    private String mWeakness;
-    private static List<Hero> instances = new ArrayList<>();
+    private String name;
+    private int age;
+    private String power;
+    private String weakness;
+    private static ArrayList<Hero> instances = new ArrayList<>();
     private int mId;
     private LocalDateTime createdAt;
 
-
-
     public Hero(String name,int age, String power, String weakness){
-        this.mName = name;
-        this.mAge = age;
-        this.mPower = power;
-        this.mWeakness = weakness;
+        this.name = name;
+        this.age = age;
+        this.power = power;
+        this.weakness = weakness;
         this.createdAt = LocalDateTime.now();
         instances.add(this);
         this.mId = instances.size();
-
     }
-
     public String getName(){
-        return this.mName;
+        return this.name;
     }
-    public int getmAge(){
-        return this.mAge;
+    public int getAge(){
+        return this.age;
     }
-    public String getmPower(){
-        return mPower;
+    public String getPower(){
+        return this.power;
     }
-    public String getmWeakness(){
-        return mWeakness;
+    public String getWeakness(){
+        return this.weakness;
     }
-    public static List<Hero> getAll(){
+    public static ArrayList<Hero> getAll(){
         return instances;
     }
-
     public static void clearAllHero(){
         instances.clear();
     }
@@ -50,8 +45,6 @@ public class Hero {
     public static Hero findById(int id){
         return instances.get(id-1);
     }
-
-
     public LocalDateTime getCreatedAt() {
         return createdAt;
     }
